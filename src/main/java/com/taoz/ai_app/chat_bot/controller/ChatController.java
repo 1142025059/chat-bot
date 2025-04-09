2fa932abda4e19b3b2cdd12a5cfc03e98488c6b0
taozFather <1142025059@qq.com>
2025-04-09 18:57:27 +0800

    初始化项目

---
+++ src/main/java/com/taoz/ai_app/chat_bot/controller/ChatController.java
@@ -0,0 +1,7 @@
+package com.taoz.ai_app.chat_bot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("chat")
+public class ChatController {
+}
---
+++ src/test/java/com/taoz/ai_app/chat_bot/ChatBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taoz.ai_app.chat_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
