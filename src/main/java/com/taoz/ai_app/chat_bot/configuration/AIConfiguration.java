95a1d9fc64195d098099fddeab09d283270aee2a
taozFather <1142025059@qq.com>
2025-04-10 04:14:46 +0800

    update .gitignore

--- src/main/java/com/taoz/ai_app/chat_bot/configuration/AIConfiguration.java
+++ src/main/java/com/taoz/ai_app/chat_bot/configuration/AIConfiguration.java
@@ -1,2 +1,18 @@
-package com.taoz.ai_app.chat_bot.configuration;public class AIConfiguration {
+package com.taoz.ai_app.chat_bot.configuration;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AIConfiguration {
+
+    @Bean
+    public ChatClient chatClient(OpenAiChatModel model) {
+        return ChatClient
+                .builder(model)
+                .build();
+    }
 }
+
