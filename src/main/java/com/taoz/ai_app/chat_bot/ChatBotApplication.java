9ca9c8d4ead8c6669b1123203cc5da66b6c726d3
taozFather <1142025059@qq.com>
2025-04-10 03:12:21 +0800

    完成后端调用大模型测试

--- src/main/java/com/taoz/ai_app/chat_bot/ChatBotApplication.java
+++ src/main/java/com/taoz/ai_app/chat_bot/ChatBotApplication.java
@@ -1,7 +1,10 @@
 package com.taoz.ai_app.chat_bot;
 
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ChatBotApplication {
@@ -9,5 +12,4 @@ public class ChatBotApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ChatBotApplication.class, args);
 	}
-
 }
--- src/main/java/com/taoz/ai_app/chat_bot/controller/ChatController.java
+++ src/main/java/com/taoz/ai_app/chat_bot/controller/ChatController.java
@@ -1,7 +1,39 @@
 package com.taoz.ai_app.chat_bot.controller;
 
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
-@RequestMapping("chat")
+import java.util.Map;
+
+@RestController
+@RequestMapping("/ai")
 public class ChatController {
+
+    private final OpenAiChatModel chatModel;
+
+    @Autowired
+    public ChatController(OpenAiChatModel chatModel) {
+        this.chatModel = chatModel;
+    }
+
+    @GetMapping("/generate")
+    public Map generate(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        return Map.of("generation", this.chatModel.call(message));
+    }
+
+    @GetMapping("/generateStream")
+    public Flux<ChatResponse> generateStream(@RequestParam(value = "message", defaultValue = "Tell me a joke") String message) {
+        Prompt prompt = new Prompt(new UserMessage(message));
+        return this.chatModel.stream(prompt);
+    }
+
+
 }
