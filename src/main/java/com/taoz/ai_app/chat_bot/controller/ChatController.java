1b9f6fdb09a654ebbcf26075def26d94d089fbc8
taoz <155210093+1142025059@users.noreply.github.com>
2025-09-19 23:51:38 +0800

    Update ChatController.java

--- src/main/java/com/taoz/ai_app/chat_bot/controller/ChatController.java
+++ src/main/java/com/taoz/ai_app/chat_bot/controller/ChatController.java
@@ -36,6 +36,8 @@ public class ChatController {
         // code6666
         // code3333
         // code4444
+        // code9999
+        // code0000
         return Map.of("generation", this.chatModel.call(message));
     }
 
